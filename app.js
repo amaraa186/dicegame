ee7a6719b5a39a88c9dae5558e5bc8df865e6797
amaraa <oamraa62@gmail.com>
2020-08-27 16:57:16 +0900

    aldaanuudaa zasah

--- app.js
+++ app.js
@@ -1,17 +1,15 @@
 //Тоглогчийн ээлжийг хадгалах хувьсагч тоглогч нэг нь 0, тоглогч хоёр нь 1 гэж тэмдэглэлээ.
-var activePlayer = 0;
+var activePlayer;
 
 //Тоглогчдын цуглуулсан оноог хадгалах хувьсагч
-var scores = [0, 0];
+var scores;
 
 //Идэвхтэй тоглогчийн ээлжийн оноог хадгалах хувьсагч
-var roundScore = 0;
-document.getElementById('current-0').textContent = "0";
-document.getElementById('current-1').textContent = "0";
+var roundScore;
 
-//DOM-оос тоглогчийн id-г олох
-document.getElementById('score-0').textContent = scores[0];
-document.getElementById('score-1').textContent = scores[1];
+// Тоглоомын бүх газарт ашиглагдах глобаль хувьсагчдыг энд зарлая
+// Тоглоом дууссан эсэхийг хадгалах төлөвийн хувьсагч
+var isNewGame;
 
 //зурагны код
 var diceDom = document.querySelector('.dice');
@@ -22,43 +20,77 @@ document.querySelector(".btn-roll").addEventListener("click", shooShid);
 document.querySelector(".btn-hold").addEventListener("click", Hadgal);
 document.querySelector(".btn-new").addEventListener("click", shineerehel);
 
+shineerehel();
+
 //functions
 function shineerehel() {
+    isNewGame = true;
+
+    activePlayer = 0;
+    scores = [0, 0];
+    roundScore = 0;
+
     document.getElementById('current-0').textContent = "0";
     document.getElementById('current-1').textContent = "0";
-    document.getElementById('score-0').textContent = "0";
-    document.getElementById('score-1').textContent = "0";
+
+    //DOM-оос тоглогчийн id-г олох
+    document.getElementById('score-0').textContent = scores[0];
+    document.getElementById('score-1').textContent = scores[1];
+
+    document.getElementById("name-0").textContent = "Player 1";
+    document.getElementById("name-1").textContent = "Player 2";
+
     diceDom.style.display = "none";
 }
 function shooShid() {
+    if(isNewGame) {
     //Шоог ямар талаараа буусныг илтгэх хувьсагч 1-6
     var diceNumber = Math.floor(Math.random() * 6) + 1;
+
+    //Шооны зургийг гаргаж ирэх
     diceDom.style.display = "block";
     diceDom.src = "dice-" + diceNumber + ".png";
+
     if(diceNumber !== 1){
+
         roundScore += diceNumber;
+
         document.getElementById('current-' + activePlayer).textContent = roundScore;
+
     } else {
-        roundScore = 0;
-        document.getElementById('current-' + activePlayer).textContent = roundScore;
-        switch (activePlayer) {
-            case 1: activePlayer = 0; break;
-            case 0: activePlayer = 1;
-        }
+        switchPlayer();
     }
+} else {
+    alert("New game товч дээр дарж тоглоомоо дахин эхлүүлээрэй.");
+}
 }
 function Hadgal() {
+    if(isNewGame) {
+
     document.getElementById('score-' + activePlayer).textContent = scores[activePlayer] += roundScore;
+
     if(scores[activePlayer] >= 100) {
-        alert("Тоглогч " + (activePlayer + 1) + " та энэ тоглолтонд яллаа.");
-        shineerehel();
+        document.getElementById('name-' + activePlayer).textContent = "WINNER!!!";
+        isNewGame = false;
     } else {
-        roundScore = 0;
+        switchPlayer();
+    }
+} else {
+    alert("New game товч дээр дарж тоглоомоо дахин эхлүүлээрэй.");
+}
+}
+
+function switchPlayer() {
+    //Улаан цэгээ арилгах үйлдэл
+    document.querySelector(".player-"+ activePlayer + "-panel").classList.remove("active");
+
+    roundScore = 0;
+
     document.getElementById('current-' + activePlayer).textContent = roundScore;
-    switch (activePlayer) {
-        case 1: activePlayer = 0; break;
-        case 0: activePlayer = 1;
-    }
-    }
+
+    activePlayer === 0 ? (activePlayer = 1) : (activePlayer = 0);
+
+    //Улаан цэгээ солих үйлдэл
+    document.querySelector(".player-"+ activePlayer + "-panel").classList.add("active");
 }
 
